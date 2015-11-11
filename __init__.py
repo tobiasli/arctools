710441b9cf830cedc40172551550a16eaccc6e5d
Tobias Litherland <tobiaslland@gmail.com>
2015-11-11 12:41:22 +0100

    Fixed __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
-from arctools import *
+from .arctools import tableToDict, dictToTable, changeFieldOrder, create_filled_contours, renameFields
+
+__all__ = ['tableToDict', 'dictToTable', 'changeFieldOrder', 'create_filled_contours', 'renameFields']
